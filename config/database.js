471bd1754e193423da96ef2b372b09ea53e656c2
tyana bandeira <tyanabandeira1@gmail.com>
2022-11-06 22:11:49 -0500

    inital commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://tyana:demoday@cluster0.tyqktud.mongodb.net/serenity?retryWrites=true&w=majority', 
+    'dbName': 'serenity'
+};
